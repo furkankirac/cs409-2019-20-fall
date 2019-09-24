cb91dc9ba60a53b394eef405f5e9ae6a2c7e1dc6
Furkan Kıraç <fkirac@gmail.com>
2019-09-24 19:07:14 +0300

    First week commit. Syllabus.

---
+++ week1-app1.cpp
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+#include <iostream>
+
+// Syllabus.
+// Midterm 25%, Final 40%, Project 35%
+// Teaching Assistant: Baris Ozcan -> Office Hour to be decided
+// Intructor Office Hours available with reservation through email
+
+// Will use:
+//   Compiler: Clang 8+ and C++17 standard. Sometimes we'll sneak into C++20 (called C++2a for now).
+//   Editor: Qt Creator 4.10+. Lightweight, directly supports CMake
+
+// COMPILER CONFORMANCE: https://en.cppreference.com/w/cpp/compiler_support
+
+// We will work on:
+// Basic C++: Structures, Classes, Enums and Constructors/Deconstructors
+// Basic Memory Management, stack, heap, static memory, std::vector usage
+// Operators, operator overloading, casting
+// Smart pointers: std::unique_ptr, std::shared_ptr, …
+// Const Correctness, const keyword, constexpr keyword
+// Midterm Exam
+// Namespaces, Literals, Custom Literals, Assertions
+// Basic Templates, auto, decltype, decltype(auto)
+// Preprocessor Tricks, Header Files, Inline Functions/Variables
+// Standard Library and important basic functions/algorithms
+// Lambda Functions and Generic Lambdas
+// Advanced Generic Programming
+
+int main(int argc, char* argv[])
+{
+//    printf("Hello World\n");
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
