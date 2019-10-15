b946f4cf92a0d4f5518724c3c04bacf4ef854e00
Furkan Kıraç <fkirac@gmail.com>
2019-10-15 18:47:14 +0300

    Using std::vector<T> as the memory allocation strategy facilitates a lot of things. Writing a const char* casting operator for ColoredPixel allows ColoredPixel instances to be pumped (<<) to std::cout.

--- week4-app1.cpp
+++ week4-app1.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 // Matrix class, Image class inherited from Matrix
 // printMatrix
 // function pointers, function objects, lambdas
 
+struct ColoredPixel
+{
+    unsigned char r, g, b;
+
+    operator const char *() const
+    {
+        static std::string s = "";
+        s = std::string("RGB=") +
+                std::to_string((int)r) + "," +
+                std::to_string((int)g) + "," +
+                std::to_string((int)b);
+        return s.c_str();
+    }
+};
 
 template<typename T>
 class Matrix
@@ -11,45 +27,38 @@ class Matrix
     size_t num_rows;
     size_t num_cols;
 
-    T* data;
+    std::vector<T> data;
 
 public:
-    Matrix() : num_rows(0), num_cols(0), data(nullptr) { }
+    Matrix() : num_rows{}, num_cols{}, data{} { }
 
     Matrix(size_t num_rows, size_t num_cols) :
         num_rows(num_rows),
         num_cols(num_cols),
-        data(new T[num_rows*num_cols])
+        data(num_rows*num_cols)
     {
-        for(size_t i=0; i<num_rows; ++i)
-            for(size_t j=0; j<num_cols; ++j)
-                (*this)(i, j) = 0;
+//        for(size_t i=0; i<num_rows; ++i)
+//            for(size_t j=0; j<num_cols; ++j)
+//                (*this)(i, j) = 0;
     }
 
-    ~Matrix() { delete[] data; }
+//    ~Matrix() { delete[] data; }
 
-    Matrix(const Matrix<T>& other) :
-        num_rows(other.num_rows),
-        num_cols(other.num_cols),
-        data(new T[other.num_rows*other.num_cols])
-    {
-        std::cout << "Copy ctor has been called." << std::endl;
-        for(size_t i=0; i<num_rows; ++i)
-            for(size_t j=0; j<num_cols; ++j)
-                (*this)(i, j) = other(i, j);
-    }
+//    Matrix(const Matrix<T>& other) :
+//        num_rows(other.num_rows),
+//        num_cols(other.num_cols),
+//        data(other.data)
+//    {
+//        std::cout << "Copy ctor has been called." << std::endl;
+//    }
 
-    void operator=(const Matrix<T>& other)
-    {
-        num_rows = other.num_rows;
-        num_cols = other.num_cols;
-        data = new T[other.num_rows*other.num_cols];
-        std::cout << "Copy assign has been called." << std::endl;
-        for(size_t i=0; i<num_rows; ++i)
-            for(size_t j=0; j<num_cols; ++j)
-                (*this)(i, j) = other(i, j);
-    }
-
+//    void operator=(const Matrix<T>& other)
+//    {
+//        num_rows = other.num_rows;
+//        num_cols = other.num_cols;
+//        data = other.data;
+//        std::cout << "Copy assign has been called." << std::endl;
+//    }
 
     void print() const
     {
@@ -61,11 +70,30 @@ public:
         }
     }
 
-    T& operator()(size_t row, size_t col) const { return data[row*num_cols + col]; }
+    const T& operator()(size_t row, size_t col) const { return data[row*num_cols + col]; }
     T& operator()(size_t row, size_t col) { return data[row*num_cols + col]; }
-
-
 };
+
+
+//typedef Matrix<ColoredPixel> ColoredImage;
+//using ColoredImage = Matrix<ColoredPixel>;
+
+class ColoredImage : public Matrix<ColoredPixel>
+{
+//    ColoredImage(size_t num_rows, size_t num_cols) :
+//        Matrix<ColoredPixel>(num_rows, num_cols)
+//    {
+//    }
+
+//    void operator=(const ColoredImage& other)
+//    {
+//        Matrix<ColoredPixel>::operator=(other);
+//    }
+
+    using Matrix<ColoredPixel>::Matrix;
+    using Matrix<ColoredPixel>::operator=;
+};
+
 
 
 int main(int argc, char* argv[])
@@ -84,6 +112,12 @@ int main(int argc, char* argv[])
     m3 = m2;
     m3.print();
 
+//    auto img0 = Matrix<ColoredPixel>(100, 100);
+    auto img1 = ColoredImage(3, 3);
+
+    img1(2, 2) = ColoredPixel{10, 100, 200};
+
+    img1.print();
 
 //    std::cout << m1(1, 1) << std::endl;
     return 0;
